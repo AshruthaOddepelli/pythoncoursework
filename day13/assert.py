5d96169ef90332e623b4f0f6bbda04ceb8ae8c92
Ashrutha Ashu <oddepelliashrutha10@gmail.com>
2026-06-11 21:46:02 +0530

    Add files via upload

---
+++ day13/assert.py
@@ -0,0 +1,12 @@
+'''
+status=True
+assert status != None,"you need to update the Status"
+print(status)
+
+'''
+
+name='ashu'
+age=22
+batch=55
+assert (name!=None and age!= None and batch!=None),"you need to update the Status"
+print(name,age,batch)
