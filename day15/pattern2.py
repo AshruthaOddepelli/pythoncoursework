82b016c9600a0872ecc73bdde78af63008a2062e
Ashrutha Ashu <oddepelliashrutha10@gmail.com>
2026-06-16 21:21:57 +0530

    Add files via upload

---
+++ day15/pattern2.py
@@ -0,0 +1,236 @@
+
+'''
+9
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+n=int(input())
+m=n//2
+for i in range(n):
+    if i<=m:
+        for j in range(i+1):
+            print('*',end='')
+    else:
+        for j in range(n-i):
+            print('*',end='')
+    print()
+
+
+#another method
+
+n=int(input())
+m=n//2
+for i in range(n):
+    if i<=m:
+        print('*'*(i+1),end='')
+    else:
+        print('*'*(n-i),end='')
+    print()
+'''
+'''
+         *  
+       * *  
+     * * *  
+   * * * *  
+ * * * * *  
+   * * * *  
+     * * *  
+       * *  
+         * 
+n=int(input())
+m=n//2
+for i in range(n):
+    if i<=m:
+        print('  '*(m-i),end=' ')
+        print('* '*(i+1),end=' ')
+    else:
+        print('  '*(i-m),end=' ')
+        print('* '*(n-i),end=' ')
+    print()
+
+'''
+'''
+     *  
+    * *  
+   * * *  
+  * * * *  
+ * * * * *  
+  * * * *  
+   * * *  
+    * *  
+     *
+n=int(input())
+m=n//2
+for i in range(n):
+    if i<=m:
+        print(' '*(m-i),end=' ')
+        print('* '*(i+1),end=' ')
+    else:
+        print(' '*(i-m),end=' ')
+        print('* '*(n-i),end=' ')
+    print()
+
+
+n=int(input())
+m=n//2
+for i in range(n):
+    if i<=m:
+        print(' '*(m-i)+'* '*(i+1),end=' ')
+    else:
+        print(' '*(i-m)+'* '*(n-i),end=' ')
+    print()
+
+'''
+'''
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j == n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+'''
+'''
+* * * * * * * * * * 
+*         *       * 
+*         *       * 
+*         *       * 
+*         *       * 
+* * * * * * * * * * 
+*         *       * 
+*         *       * 
+*         *       * 
+* * * * * * * * * *
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1 or i==n//2 or j==n//2:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+
+'''
+'''
+* * * * * * * * * * 
+* *             * * 
+*   *         *   * 
+*     *     *     * 
+*       * *       * 
+*       * *       * 
+*     *     *     * 
+*   *         *   * 
+* *             * * 
+* * * * * * * * * * 
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1 or j==i or i+j==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+
+'''
+'''
+n=int(input())
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==m or j==0 or j == n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+'''
+'''
+n=int(input())
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==m or j==0 or j == n-1 or i == n-1    :
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+'''
+'''
+n=int(input())
+
+for i in range(n):
+    for j in range(n):
+        if i==0  or j==0 or i == n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+
+n=int(input())
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if i==0  or j==0 or i == m:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+n=int(input())
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if i==0  or j==0 or i == m or i==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+
+n=int(input())
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if j==0  or  i == m or j==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+n=int(input())
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if i==0  or  j == m or i==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
+
+'''
+n=int(input())
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if i==0  or  j == m or j>=m or i>=m o :
+            print('*',end=' ')
+        else:
+            print(' ',end=' ') 
+    print()
